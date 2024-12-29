1de1a5d823d5ee802d24dac839ceb66138df8d10
Mahdeen Khan <mahdeen.s.khan@yale.edu>
2024-12-29 00:10:39 -0500

    new font and small changes

--- src/components/Content/Pages/One.tsx
+++ src/components/Content/Pages/One.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styles from "styles/Page.module.css";
 
 export default function One() {
   return (
-    <p>
-      There once was a lady named Katie, and she was just like a fairy. She{" "}
+    <p className={styles.text}>
+      <span className={styles.firstWord}>
+        <span className={styles.firstLetter}>T</span>here
+      </span>{" "}
+      once was a lady named Katie, and she was just like a fairy. She
     </p>
   );
 }
